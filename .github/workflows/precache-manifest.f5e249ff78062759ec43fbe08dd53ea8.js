e973e291e0923cbf9bf98a67d5be9a4b1b7beb73
Venky12121212 <60850781+Venky12121212@users.noreply.github.com>
2020-06-21 12:50:53 +0530

    Add files via upload

---
+++ .github/workflows/precache-manifest.f5e249ff78062759ec43fbe08dd53ea8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab2986433d1ddfddb3f0795f41b29be6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85889abfaa136cc8bfb9",
+    "url": "/static/css/main.04fc0cb9.chunk.css"
+  },
+  {
+    "revision": "91e38d1e076b4eda639e",
+    "url": "/static/js/2.66966139.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/static/js/2.66966139.chunk.js.LICENSE"
+  },
+  {
+    "revision": "85889abfaa136cc8bfb9",
+    "url": "/static/js/main.d11919f5.chunk.js"
+  },
+  {
+    "revision": "f718a062546416d5bccd",
+    "url": "/static/js/runtime-main.c5b1e994.js"
+  },
+  {
+    "revision": "0836a7092d9abdd9cb606306ccee3159",
+    "url": "/static/media/loader.0836a709.gif"
+  }
+]);
